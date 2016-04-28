471597531340e8667055aafd013709f7e8ed17a5
turkdice <vermessenverme@gmail.com>
2016-04-29 02:04:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfJZx4TAAAAADAu0JG6toruHcWuQ6wP9LJowNk0',  // <----- EDIT ME!
-  redirect_uri: 'http://turkdice.github,io,
+  redirect_uri: 'http://turkdice.github,io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
