f70f7d92a868678b096467bd188e207f5582d42b
turkdice <vermessenverme@gmail.com>
2016-04-29 02:44:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 1099,                           // <----------------------------- EDIT ME!
+  app_id: 1102,                           // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Turk Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfJZx4TAAAAADAu0JG6toruHcWuQ6wP9LJowNk0',  // <----- EDIT ME!
-  redirect_uri: 'http://turkdice.github,io',
+  redirect_uri: 'http://turkdice.com,
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
