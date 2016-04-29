d2f92bdd28e1934f220b98a9900df5371ed568e5
turkdice <vermessenverme@gmail.com>
2016-04-29 04:11:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 1099,                           // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Turk Dice',
+  app_name: 'Turkish Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfJZx4TAAAAADAu0JG6toruHcWuQ6wP9LJowNk0',  // <----- EDIT ME!
